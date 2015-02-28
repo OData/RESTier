5161a9fdc8d7c95386b66e67b4cd46f6834cae63
Dong Liu <doliu@microsoft.com>
2015-02-28 11:30:35 +0800

    Uniform license header

--- src/GlobalSuppressions.cs
+++ src/GlobalSuppressions.cs
@@ -1,3 +1,6 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames")]
