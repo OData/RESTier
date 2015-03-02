d8299f6e91d101ed18c6c7985ee38fab18504e99
Dong Liu <doliu@microsoft.com>
2015-03-02 17:17:26 +0800

    Fix FxCop issue for Microsoft.Restier.EntityFramework

--- src/Microsoft.Restier.EntityFramework/Model/ModelProducer.cs
+++ src/Microsoft.Restier.EntityFramework/Model/ModelProducer.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Core.Metadata.Edm;
@@ -51,6 +52,7 @@ namespace Microsoft.Restier.EntityFramework.Model
             ModelContext context,
             CancellationToken cancellationToken)
         {
+            Ensure.NotNull(context);
             var model = new EdmModel();
             var domainContext = context.DomainContext;
             var dbContext = domainContext.GetProperty<DbContext>("DbContext");
--- src/Microsoft.Restier.EntityFramework/Query/QueryExpressionSourcer.cs
+++ src/Microsoft.Restier.EntityFramework/Query/QueryExpressionSourcer.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
+using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
@@ -37,6 +38,7 @@ namespace Microsoft.Restier.EntityFramework.Query
         /// </returns>
         public Expression Source(QueryExpressionContext context, bool embedded)
         {
+            Ensure.NotNull(context);
             var dbContext = context.QueryContext
                 .DomainContext.GetProperty<DbContext>("DbContext");
             var dbSetProperty = dbContext.GetType().GetProperties()
