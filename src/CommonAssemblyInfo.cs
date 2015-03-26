e57cb1af8300136c03a9668c000fed2b90afd8ed
layliu <layliu@microsoft.com>
2015-03-26 16:19:31 +0800

    Make assemblies servicesable

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -13,3 +13,4 @@ using System.Resources;
 [assembly: AssemblyVersion("0.2.0.0")]
 [assembly: AssemblyFileVersion("0.2.0.0")]
 [assembly: AssemblyInformationalVersion("0.2.0-pre")]
+[assembly: AssemblyMetadata("Serviceable", "True")]
