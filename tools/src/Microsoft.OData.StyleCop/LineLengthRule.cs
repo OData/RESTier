ef7f94af6039b511674059e0bf54cc18f6f9d252
Lewis Cheng <licheng@microsoft.com>
2015-07-09 14:11:33 +0800

    Add Microsoft.OData.StyleCop

---
+++ tools/src/Microsoft.OData.StyleCop/LineLengthRule.cs
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using StyleCop;
+using StyleCop.CSharp;
+
+namespace Microsoft.OData.StyleCop
+{
+    [SourceAnalyzer(typeof(CsParser))]
+    public class LineLengthRule : SourceAnalyzer
+    {
+        private const string LineLengthProperty = "LineLength";
+        private const string RuleName = "LineMustNotBeTooLong";
+
+        public override void AnalyzeDocument(CodeDocument document)
+        {
+            var csharpDocument = (CsDocument)document;
+            if (csharpDocument.RootElement != null && !csharpDocument.RootElement.Generated)
+            {
+                if (IsRuleEnabled(csharpDocument, RuleName))
+                {
+                    CheckLineLength(csharpDocument);
+                }
+            }
+        }
+
+        private void CheckLineLength(CsDocument csharpDocument)
+        {
+            var lengthProperty = GetSetting(csharpDocument.Settings, LineLengthProperty) as IntProperty;
+            if (lengthProperty == null)
+            {
+                return;
+            }
+
+            using (var reader = csharpDocument.SourceCode.Read())
+            {
+                string line;
+                var lineNumber = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lineNumber++;
+                    if (line.Length > lengthProperty.Value)
+                    {
+                        AddViolation(
+                            csharpDocument.RootElement, 
+                            lineNumber, 
+                            RuleName,
+                            lengthProperty.Value);
+                    }
+                }
+            }
+        }
+    }
+}
