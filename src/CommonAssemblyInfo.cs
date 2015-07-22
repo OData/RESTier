ae0b6ae300d79edc24a0348960431a4128b7dd7d
Dong Liu <doliu@microsoft.com>
2015-07-22 16:58:08 +0800

    Update version to 0.3.0-beta

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Resources;
 #endif
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
-[assembly: AssemblyInformationalVersion("0.2.0-pre")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyInformationalVersion("0.3.0-beta")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
