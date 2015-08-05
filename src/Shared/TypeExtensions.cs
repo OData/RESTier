811445f6a66637b9d790ae1f996c9270da82e5af
mkemal <mkemalkorkmaz@gmail.com>
2015-08-05 12:17:59 +0300

    Some optimization to TypeExtensions.FindGenericType method

--- src/Shared/TypeExtensions.cs
+++ src/Shared/TypeExtensions.cs
@@ -13,32 +13,80 @@ namespace System
                                                                  BindingFlags.IgnoreCase |
                                                                  BindingFlags.DeclaredOnly;
 
+
+        /// <summary>
+        /// Find a base type or implemented interface which has a generic definition
+        /// represented by the parameter, <c>definition</c>.
+        /// </summary>
+        /// <param name="type">
+        /// The subject type.
+        /// </param>
+        /// <param name="definition">
+        /// The generic definiton to check with.
+        /// </param>
+        /// <returns>
+        /// The base type or the interface found; otherwise, <c>null</c>.
+        /// </returns>
         public static Type FindGenericType(this Type type, Type definition)
         {
-            while (type != null && type != typeof(object))
+            // If the type conforms the given generic definition, no further check required.
+            if (type.HasGenericDefinition(definition))
             {
-                if (type.IsGenericType &&
-                    type.GetGenericTypeDefinition() == definition)
-                {
-                    return type;
-                }
+                return type;
+            }
 
-                if (definition.IsInterface)
+            // If the definition is interface, we only need to check the interfaces 
+            // implemented by the current type
+            if (definition.IsInterface)
+            {
+                foreach (var interfaceType in type.GetInterfaces())
                 {
-                    foreach (var interfaceType in type.GetInterfaces())
+                    if (interfaceType.HasGenericDefinition(definition))
                     {
-                        var found = interfaceType.FindGenericType(definition);
-                        if (found != null)
-                        {
-                            return found;
-                        }
+                        return interfaceType;
                     }
                 }
+            }
 
-                type = type.BaseType;
+            // If the definition is not an interface, then the current type cannot be an interface too.
+            // Otherwise, we should only check the parent class types of the current type.
+            else if (!type.IsInterface)
+            {
+                // no null check for the type required, as we are sure it is not an interface type
+                while (type != typeof (object))
+                {
+                    if (type.HasGenericDefinition(definition))
+                    {
+                        return type;
+                    }
+
+                    type = type.BaseType;
+                }
+
             }
 
             return null;
+        }
+
+        /// <summary>
+        /// Check if this type has a generic definition 
+        /// represented by the parameter, <c>definition</c>.
+        /// </summary>
+        /// <param name="type">
+        /// The subject type.
+        /// </param>
+        /// <param name="definition">
+        /// The generic definiton to check with.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the type is genereic and has a generic definition 
+        /// represented by the parameter, <c>definition</c>; 
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        public static bool HasGenericDefinition(this Type type, Type definition)
+        {
+            return type.IsGenericType &&
+                   type.GetGenericTypeDefinition() == definition;
         }
 
         public static MethodInfo GetQualifiedMethod(this Type type, string methodName)
