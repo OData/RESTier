6883b62cee8449ec15180863e7cb603e46ebcf1c
Dong Liu <doliu@microsoft.com>
2015-08-17 13:48:01 +0800

    Add TrippinE2ETestBase

---
+++ test/ODataEndToEndTests/Microsoft.Restier.WebApi.Test.Scenario/TrippinE2ETestBase.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Restier.WebApi.Test.Services.Trippin.Models;
+using Xunit;
+
+namespace Microsoft.Restier.WebApi.Test.Scenario
+{
+    public abstract class TrippinE2ETestBase : E2ETestBase<TrippinModel>, IClassFixture<TrippinServiceFixture>
+    {
+        protected TrippinE2ETestBase()
+            : base(new Uri("http://localhost:18384/api/Trippin/"))
+        {
+        }
+    }
+}
