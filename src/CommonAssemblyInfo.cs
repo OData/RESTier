2c0b4cdecedaded7e5fe20f334acd226d1106939
Lewis Cheng <licheng@microsoft.com>
2016-03-08 09:47:35 +0800

    Bump version to 0.5.0-beta

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Resources;
 #endif
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
-[assembly: AssemblyInformationalVersion("0.4.0-rc2")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyInformationalVersion("0.5.0-beta")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
