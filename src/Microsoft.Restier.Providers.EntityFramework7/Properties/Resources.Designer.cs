0fcd8f00edaa2f229e80476cfe23a1fbc513edda
Vincent He <dashe@microsoft.com>
2016-05-23 13:34:07 +0800

    Change assemble name

--- src/Microsoft.Restier.Providers.EntityFramework7/Properties/Resources.Designer.cs
+++ src/Microsoft.Restier.Providers.EntityFramework7/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.Restier.EntityFramework.Properties {
+namespace Microsoft.Restier.Providers.EntityFramework.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Microsoft.Restier.EntityFramework.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Restier.EntityFramework.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Restier.Providers.EntityFramework.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
