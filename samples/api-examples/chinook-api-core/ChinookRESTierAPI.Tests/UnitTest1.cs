71cf674e7bc5e9c84093e0548b4cd2eadd3a0ff5
Chris Woodruff <cwoodruff@live.com>
2019-03-18 21:38:33 -0400

    Added a working RESTier project. No unit tests set up yet

---
+++ samples/api-examples/chinook-api-core/ChinookRESTierAPI.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChinookRESTierAPI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
