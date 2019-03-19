3e6bfb16c32ce37f4107925f98e8f02fa7d4d371
Chris Woodruff <cwoodruff@live.com>
2019-03-18 21:38:33 -0400

    Added a working RESTier project. No unit tests set up yet

---
+++ samples/api-examples/chinook-api-core/ChinookRESTierAPI.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChinookRESTierAPI.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChinookRESTierAPI.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("41bac0cc-c2b0-47b8-91c6-8e0e9fcbcf44")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ samples/api-examples/chinook-api-core/ChinookRESTierAPI/Global.asax.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace ChinookRESTierAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
