87246ea315f68cfd6a4e4a235f3715159f68c1bc
Robert McLaws <robert@nimbleapps.cloud>
2019-09-15 21:19:33 -0400

    - Updating to latest OData packages - Adding Authenticode signing - Removing legacy test projects.

--- test/ODataEndToEnd/Microsoft.OData.Service.Sample.Trippin/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Microsoft.OData.Service.Sample.Trippin.WebApiApplication" Language="C#" %>
--- test/ODataEndToEnd/Microsoft.OData.Service.Sample.Trippin/Models/Sponsor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microsoft.OData.Service.Sample.Trippin.Models
-{
-    public class Sponsor
-    {
-        public int SponsorId { get; set; }
-
-        public string Name { get; set; }
-    }
-}
