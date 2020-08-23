6e3080f045c4fe2ca0b84421d8b77e171096d59c
Robert McLaws <robert@nimbleapps.cloud>
2020-08-23 19:14:32 -0400

    Update BatchTests.cs
    Testing Functions in Batches.

--- src/Microsoft.Restier.Tests.AspNet/FeatureTests/BatchTests.cs
+++ src/Microsoft.Restier.Tests.AspNet/FeatureTests/BatchTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -83,6 +82,75 @@ namespace Microsoft.Restier.Tests.AspNet.FeatureTests
             content.Should().Contain("1111111111111");
             content.Should().Contain("2222222222222");
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task BatchTests_SelectPlusFunctionResult()
+        {
+            var config = await RestierTestHelpers.GetTestableRestierConfiguration<LibraryApi, LibraryContext>().ConfigureAwait(false);
+            var httpClient = config.GetTestableHttpClient();
+            httpClient.BaseAddress = new Uri($"{WebApiConstants.Localhost}{WebApiConstants.RoutePrefix}");
+
+            var odataSettings = new ODataClientSettings(httpClient, new Uri("", UriKind.Relative))
+            {
+                OnTrace = (x, y) => TestContext.WriteLine(string.Format(x, y)),
+                // RWM: Need a batter way to capture the payload... this event fires before the payload is written to the stream.
+                //BeforeRequestAsync = async (x) => {
+                //    var ms = new MemoryStream();
+                //    if (x.Content != null)
+                //    {
+                //        await x.Content.CopyToAsync(ms).ConfigureAwait(false);
+                //        var streamContent = new StreamContent(ms);
+                //        var request = await streamContent.ReadAsStringAsync();
+                //        TestContext.WriteLine(request);
+                //    }
+                //},
+                //AfterResponseAsync = async (x) => TestContext.WriteLine(await x.Content.ReadAsStringAsync()),
+            };
+
+            var odataBatch = new ODataBatch(odataSettings);
+            var odataClient = new ODataClient(odataSettings);
+
+            Publisher publisher = null;
+            Book book = null;
+
+            odataBatch += async c =>
+                publisher = await odataClient
+                    .For<Publisher>()
+                    .Key("Publisher1")
+                    .FindEntryAsync();
+
+            odataBatch += async c =>
+            {
+                book = await c
+                    .Unbound<Book>()
+                    .Function("PublishBook")
+                    .Set(new { IsActive = true })
+                    .ExecuteAsSingleAsync();
+            };
+
+            //RWM: This way should also work.
+            //var payload = odataBatch.ToString();
+
+            try
+            {
+                await odataBatch.ExecuteAsync();
+            }
+            catch (WebRequestException exception)
+            {
+                TestContext.WriteLine(exception.Response);
+                throw;
+            }
+
+            publisher.Should().NotBeNull();
+            publisher.Addr.Zip.Should().Be("00010");
+            book.Should().NotBeNull();
+            book.Title.Should().Be("The Cat in the Hat");
+        }
+
 
     }
 
