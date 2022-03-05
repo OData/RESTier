8fc1d60889454a7afcba1494fcf464a7e071c1e9
mikepizzo <mikep@microsoft.com>
2022-03-05 10:42:00 -0800

    Added test to validate batch request/response payloads

--- src/Microsoft.Restier.Tests.AspNet/FeatureTests/BatchTests.cs
+++ src/Microsoft.Restier.Tests.AspNet/FeatureTests/BatchTests.cs
@@ -105,6 +105,90 @@ namespace Microsoft.Restier.Tests.AspNet.FeatureTests
         }
 
         /// <summary>
+        /// Validates batch request and response payloads
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task BatchTests_PayloadTest()
+        {
+#if NETCOREAPP3_1_OR_GREATER
+            var httpClient = await RestierTestHelpers.GetTestableHttpClient<LibraryApi>(serviceCollection: services => services.AddEntityFrameworkServices<LibraryContext>());
+#else
+            var config = await RestierTestHelpers.GetTestableRestierConfiguration<LibraryApi>(serviceCollection: services => services.AddEntityFrameworkServices<LibraryContext>()).ConfigureAwait(false);
+            var httpClient = config.GetTestableHttpClient();
+#endif
+
+            httpClient.BaseAddress = new Uri("http://localhost/api/tests/");
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "$batch");
+            request.Content = new StringContent(batchRequest);
+            request.Content.Headers.ContentType = MediaTypeWithQualityHeaderValue.Parse("multipart/mixed;boundary=batch_2e6281b5-fc5f-47c1-9692-5ad43fa6088b");
+
+            var response = httpClient.SendAsync(request).Result;
+            var content = await TestContext.LogAndReturnMessageContentAsync(response);
+
+            response.IsSuccessStatusCode.Should().BeTrue();
+            content.Should().Contain(batchResponse1);
+            content.Should().Contain(batchResponse2);
+        }
+
+        string batchRequest =
+@"--batch_2e6281b5-fc5f-47c1-9692-5ad43fa6088b
+Content-Type: multipart/mixed;boundary=changeset_ee671721-3d96-462d-ac58-67530e4b530c
+
+--changeset_ee671721-3d96-462d-ac58-67530e4b530c
+Content-Type: application/http
+Content-Transfer-Encoding: binary
+Content-ID: 1
+
+POST http://localhost/api/tests/Books HTTP/1.1
+Content-ID: 1
+Prefer: return=representation
+OData-Version: 4.0
+Content-Type: application/json;odata.metadata=minimal;odata.streaming=true;IEEE754Compatible=false;charset=utf-8
+
+{""@odata.type"":""#Microsoft.Restier.Tests.Shared.Scenarios.Library.Book"",""Id"":""79874b37-ce46-4f4c-aa74-8e02ce4d8b67"",""Isbn"":""1111111111111"",""Title"":""Batch Test #1"",""IsActive"":true,""Publisher@odata.bind"":""http://localhost/api/tests/Publishers(%27Publisher1%27)""}
+--changeset_ee671721-3d96-462d-ac58-67530e4b530c
+Content-Type: application/http
+Content-Transfer-Encoding: binary
+Content-ID: 2
+
+POST http://localhost/api/tests/Books HTTP/1.1
+Content-ID: 2
+Prefer: return=representation
+OData-Version: 4.0
+Content-Type: application/json;odata.metadata=minimal;odata.streaming=true;IEEE754Compatible=false;charset=utf-8
+
+{""@odata.type"":""#Microsoft.Restier.Tests.Shared.Scenarios.Library.Book"",""Id"":""c6b67ec7-badc-45c6-98c7-c76b570ce694"",""Isbn"":""2222222222222"",""Title"":""Batch Test #2"",""IsActive"":true,""Publisher@odata.bind"":""http://localhost/api/tests/Publishers(%27Publisher1%27)""}
+--changeset_ee671721-3d96-462d-ac58-67530e4b530c--
+--batch_2e6281b5-fc5f-47c1-9692-5ad43fa6088b--
+";
+
+        string batchResponse1 =
+@"Content-Type: application/http
+Content-Transfer-Encoding: binary
+Content-ID: 1
+
+HTTP/1.1 201 Created
+Location: http://localhost/api/tests/Books(79874b37-ce46-4f4c-aa74-8e02ce4d8b67)
+Content-Type: application/json; odata.metadata=minimal; odata.streaming=true
+OData-Version: 4.0
+
+{""@odata.context"":""http://localhost/api/tests/$metadata#Books/$entity"",""Id"":""79874b37-ce46-4f4c-aa74-8e02ce4d8b67"",""Isbn"":""1111111111111"",""Title"":""Batch Test #1"",""IsActive"":true}
+";
+        string batchResponse2 =
+@"Content-Type: application/http
+Content-Transfer-Encoding: binary
+Content-ID: 2
+
+HTTP/1.1 201 Created
+Location: http://localhost/api/tests/Books(c6b67ec7-badc-45c6-98c7-c76b570ce694)
+Content-Type: application/json; odata.metadata=minimal; odata.streaming=true
+OData-Version: 4.0
+
+{""@odata.context"":""http://localhost/api/tests/$metadata#Books/$entity"",""Id"":""c6b67ec7-badc-45c6-98c7-c76b570ce694"",""Isbn"":""2222222222222"",""Title"":""Batch Test #2"",""IsActive"":true}
+";
+
+        /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
