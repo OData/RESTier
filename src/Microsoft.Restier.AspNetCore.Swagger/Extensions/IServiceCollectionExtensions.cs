20820df50e5a3aa9575ea6ee89fa940a8b9d78ae
Robert McLaws <1657085+robertmclaws@users.noreply.github.com>
2023-11-25 17:04:46 -0500

    Implementing Official Swagger Support - Inject the RestierRouteBuilder so it can be accessed by the Swagger setup. - Implement version of https://github.com/OData/RESTier/issues/720#issuecomment-1826147986 - Automagically configure OpenApi to handle the endpoints correctly.

---
+++ src/Microsoft.Restier.AspNetCore.Swagger/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using Microsoft.OpenApi.OData;
+using Microsoft.Restier.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Restier_AspNetCore_Swagger_IServiceCollectionExtensions
+    {
+
+        /// <summary>
+        /// Adds the required services to use Swagger with Restier.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to register Swagger services with.</param>
+        /// <param name="openApiSettings">An <see cref="Action{OpenApiConvertSettings}"/> that allows you to configure the core Swagger output.</param>
+        /// <returns></returns>
+        public static IServiceCollection AddRestierSwagger(this IServiceCollection services, Action<OpenApiConvertSettings> openApiSettings = null)
+        {
+            services.AddScoped<ISwaggerProvider, RestierSwaggerProvider>();
+            if (openApiSettings is not null)
+            {
+                services.AddScoped(sp => openApiSettings);
+            }
+            return services;
+        }
+
+    }
+
+}
---
+++ src/Microsoft.Restier.AspNetCore.Swagger/RestierSwaggerProvider.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OData.Edm;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.OData;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+
+namespace Microsoft.Restier.AspNetCore.Swagger
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RestierSwaggerProvider : ISwaggerProvider
+    {
+
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly IPerRouteContainer perRouteContainer;
+        private readonly Action<OpenApiConvertSettings> openApiSettings;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="httpContextAccessor"></param>
+        /// <param name="perRouteContainer"></param>
+        /// <param name="openApiSettings"></param>
+        public RestierSwaggerProvider(IHttpContextAccessor httpContextAccessor, IPerRouteContainer perRouteContainer, Action<OpenApiConvertSettings> openApiSettings = null)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+            this.perRouteContainer = perRouteContainer;
+            this.openApiSettings = openApiSettings;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="documentName"></param>
+        /// <param name="host"></param>
+        /// <param name="basePath"></param>
+        /// <returns></returns>
+        public OpenApiDocument GetSwagger(string documentName, string host = null, string basePath = null)
+        {
+            var model = perRouteContainer
+                .GetODataRootContainer(documentName)
+                .GetRequiredService<IEdmModel>();
+
+            // @robertmclaws: If the user registered a custom OpenApiConvertSettings, use it.
+            var settings = new OpenApiConvertSettings();
+            openApiSettings?.Invoke(settings);
+
+            // @robertmclaws: The host defaults internally to localhost; isn't set automatically.
+            var requestHost = httpContextAccessor.HttpContext.Request.Host.Value;
+            if (!settings.ServiceRoot.ToString().StartsWith(requestHost))
+            {
+                settings.ServiceRoot = new Uri($"{httpContextAccessor.HttpContext.Request.Scheme}://{requestHost}");
+            }
+
+            return model.ConvertToOpenApi(settings);
+        }
+
+    }
+
+}
