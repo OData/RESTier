83f0c76c276f762d2360774b793803670e6e865f
mitch.kuijpers@nocore.nl <mitch.kuijpers@nocore.nl>
2024-07-04 15:32:35 +0200

    Also changed the Aspnet dictionary

--- src/Microsoft.Restier.AspNet/Batch/RestierBatchChangeSetRequestItem.cs
+++ src/Microsoft.Restier.AspNet/Batch/RestierBatchChangeSetRequestItem.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -54,7 +55,7 @@ namespace Microsoft.Restier.AspNet.Batch
             };
             SetChangeSetProperty(changeSetProperty);
 
-            var contentIdToLocationMapping = new Dictionary<string, string>();
+            var contentIdToLocationMapping = new ConcurrentDictionary<string, string>();
             var responseTasks = new List<Task<Task<HttpResponseMessage>>>();
 
             foreach (var request in Requests)
